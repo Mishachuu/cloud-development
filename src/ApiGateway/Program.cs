41eaef425342f1e6dac75653dc43f131cb2b7429
Mishachuu <mishakrut11@gmail.com>
2026-04-16 11:30:01 +0400

    fixx

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -8,26 +8,21 @@ builder.AddServiceDefaults();
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-var ocelotConfig = builder.Configuration.GetSection("Routes:0");
-var hosts = new List<object>();
+var urls = new List<Uri>();
 
 for (var i = 1; ; i++)
 {
     var url = builder.Configuration[$"services__vehicleapi-{i}__http__0"];
     if (url == null) break;
-
-    var uri = new Uri(url);
-    hosts.Add(new { Host = uri.Host, Port = uri.Port });
+    urls.Add(new Uri(url));
 }
 
-if (hosts.Count > 0)
+if (urls.Count > 0)
 {
-    builder.Configuration["Routes:0:DownstreamHostAndPorts"] = null;
-    for (var i = 0; i < hosts.Count; i++)
+    for (var i = 0; i < urls.Count; i++)
     {
-        var h = (dynamic)hosts[i];
-        builder.Configuration[$"Routes:0:DownstreamHostAndPorts:{i}:Host"] = h.Host;
-        builder.Configuration[$"Routes:0:DownstreamHostAndPorts:{i}:Port"] = h.Port.ToString();
+        builder.Configuration[$"Routes:0:DownstreamHostAndPorts:{i}:Host"] = urls[i].Host;
+        builder.Configuration[$"Routes:0:DownstreamHostAndPorts:{i}:Port"] = urls[i].Port.ToString();
     }
 }
 
